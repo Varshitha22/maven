15ad3dbba1ea45c5d38fbe8c4ac05a10d477e774
Varshitha22 <46986368+Varshitha22@users.noreply.github.com>
2019-06-06 17:41:46 +0530

    maven is connected

---
+++ src/main/java/varshitha1/Maven1.java
@@ -0,0 +1,24 @@
+package varshitha1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Maven1 {
+	@Test
+	public void Test()
+	{
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.seleniumhq.org/");
+		System.out.println(driver.getTitle());
+		driver.findElement(By.xpath("//a[@title='Get Selenium']")).click();
+		System.out.println(driver.getTitle());
+		driver.close();
+		
+		
+		}
+}
